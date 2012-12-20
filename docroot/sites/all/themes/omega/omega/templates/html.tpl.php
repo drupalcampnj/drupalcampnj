9cb5ef66588ac44da2891bce15933d777c1f352c
Michael Muzzie <mdmuzzie@princeton.edu>
2012-12-19 23:52:10 -0500

    disable IE Compatibility View

--- docroot/sites/all/themes/omega/omega/templates/html.tpl.php
+++ docroot/sites/all/themes/omega/omega/templates/html.tpl.php
@@ -2,7 +2,8 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title><?php print $head_title; ?></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
