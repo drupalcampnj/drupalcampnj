312af4c814f0cdd2c37b6d0414231b3224e74dff
Michael Muzzie <mdmuzzie@princeton.edu>
2012-12-20 01:32:55 -0500

    disable IE Compatibility View 6

--- docroot/sites/all/themes/drupalcampnj/templates/html.tpl.php
+++ docroot/sites/all/themes/drupalcampnj/templates/html.tpl.php
@@ -1,16 +1,12 @@
 <?php print $doctype; ?>
-<!--[if lt IE 7]> <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?> class="ie6"> <![endif]-->
-<!--[if IE 7]> <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?> class="ie7"> <![endif]-->
-<!--[if gt IE 8]><!-->
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
-<!-- <![endif]-->
 <head<?php print $rdf->profile; ?>>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
