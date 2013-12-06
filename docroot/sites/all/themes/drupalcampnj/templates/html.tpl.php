2e3e5634aeaf63d6e83c312e619bb86a48a91dee
Sean T. Walsh <sean@crowdcg.com>
2013-12-05 22:41:10 -0500

    update the default theme logo to the tiger and ad og:image meta

--- docroot/sites/all/themes/drupalcampnj/templates/html.tpl.php
+++ docroot/sites/all/themes/drupalcampnj/templates/html.tpl.php
@@ -2,6 +2,7 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta property="og:image" content="logo.png"/>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
