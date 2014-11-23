82a6f116dad1c2e425dc43ab4f8ef16b116dc8d1
pwolanin <pwolanin@49851.no-reply.drupal.org>
2014-11-23 16:26:10 -0500

    add cloud hook and amillog module

---
+++ hooks/common/post-db-copy/dev-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Cloud Hook: post-db-copy
+#
+# The post-db-copy hook is run whenever you use the Workflow page to copy a
+# database from one environment to another. See ../README.md for
+# details.
+#
+# Usage: post-db-copy site target-env db-name source-env
+
+site="$1"
+target_env="$2"
+db_name="$3"
+source_env="$4"
+
+echo "$site.$target_env: Received copy of database $db_name from $source_env."
+
+if [ "$target_env" = "prod" ]; then exit; fi
+
+echo "Enable & configure maillog module so we don't send mail."
+drush6 @$site.$target_env vset --exact -y maillog_devel 0
+drush6 @$site.$target_env vset --exact -y maillog_log 1
+drush6 @$site.$target_env vset --exact -y maillog_send 0
+
+drush6 @$site.$target_env -y en maillog
