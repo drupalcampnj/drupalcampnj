c586f7f131d23d92bb1ebdb7f4551129afca1759
pwolanin <pwolanin@49851.no-reply.drupal.org>
2015-01-16 15:39:16 -0500

    cleanup duplicate code in settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -552,15 +552,6 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  */
 # $conf['allow_authorize_operations'] = FALSE;
 
-
-// On Acquia Cloud, this include file configures Drupal to use the correct
-// database in each site environment (Dev, Stage, or Prod). To use this
-// settings.php for development on your local workstation, set $db_url
-// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
-if (file_exists('/var/www/site-php')) {
-}
-
-
 // On Acquia Cloud, this include file configures Drupal to use the correct
 // database in each site environment (Dev, Stage, or Prod). To use this
 // settings.php for development on your local workstation, set $db_url
