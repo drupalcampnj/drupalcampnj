bc40f6ae0107cf96d61616e83183651a2471c018
Levi Sigworth <levis@alumni.cmu.edu>
2015-10-23 15:33:33 -0400

    Create Footer content and place it in the Footer.

---
+++ docroot/sites/default/modules/custom/njblocks/templates/footer_contact_info.tpl.php
@@ -0,0 +1,23 @@
+<?php ?>
+
+<p>Get in touch with us if you are interested in sponsoring, speaking, or volunteering.</p>
+
+<ul>
+	<li><a href="tel:609-831-3265">609-831-DCNJ(3265)</a></li>
+	<li><a href="mailto:team@drupalcampnj.org">team@drupalcampnj.org</a></li>
+</ul>
+
+<ul>
+	<li><a href="http://www.meetup.com/Central-NJ-Drupal-Group/">MeetUp</a></li>
+	<li><a href="http://www.twitter.com/drupalcampnj/">Twitter</a></li>
+	<li><a href="https://groups.drupal.org/new-jersey">NJ Drupal Group</a></li>
+</ul>
+
+<h4>DrupalCamp NJ Archives</h4>
+
+<ul>
+	<li><a href="http://drupalcampnj2015.drupalcamp.org">2015</a></li>
+	<li><a href="http://drupalcampnj2014.drupalcamp.org">2014</a></li>
+	<li><a href="http://drupalcampnj2013.drupalcamp.org">2013</a></li>
+	<li><a href="http://drupalcampnj2012.drupalcamp.org/">2012</a></li>
+</ul>
