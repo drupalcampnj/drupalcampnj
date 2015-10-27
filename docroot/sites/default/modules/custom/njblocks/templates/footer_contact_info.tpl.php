d21e79d10d41b673c90f8779669777e0b77896a7
Sean T. Walsh <sean@crowdcg.com>
2015-10-26 22:33:28 -0400

    adjust footer to include icons

--- docroot/sites/default/modules/custom/njblocks/templates/footer_contact_info.tpl.php
+++ docroot/sites/default/modules/custom/njblocks/templates/footer_contact_info.tpl.php
@@ -3,14 +3,14 @@
 <p>Get in touch with us if you are interested in sponsoring, speaking, or volunteering.</p>
 
 <ul>
-	<li><a href="tel:609-831-3265">609-831-DCNJ(3265)</a></li>
-	<li><a href="mailto:team@drupalcampnj.org">team@drupalcampnj.org</a></li>
+	<li><a href="tel:609-831-3265"><i title="icon-phone" class="icon fontello icon-phone" aria-hidden="true"></i><br>609-831-DCNJ(3265)</a></li>
+	<li><a href="mailto:team@drupalcampnj.org"><i title="icon-mail" class="icon fontello icon-mail" aria-hidden="true"></i><br>team@drupalcampnj.org</a></li>
 </ul>
 
 <ul>
-	<li><a href="http://www.meetup.com/Central-NJ-Drupal-Group/">MeetUp</a></li>
-	<li><a href="http://www.twitter.com/drupalcampnj/">Twitter</a></li>
-	<li><a href="https://groups.drupal.org/new-jersey">NJ Drupal Group</a></li>
+	<li><a href="http://www.meetup.com/Central-NJ-Drupal-Group/"><i title="icon-comment" class="icon fontello icon-comment" aria-hidden="true"></i></a></li>
+	<li><a href="http://www.twitter.com/drupalcampnj/"><i title="icon-twitter" class="icon fontello icon-twitter" aria-hidden="true"></i></a></li>
+	<li><a href="https://groups.drupal.org/new-jersey"><i title="icon-drupal" class="icon fontello icon-drupal" aria-hidden="true"></i></a></li>
 </ul>
 
 <h4>DrupalCamp NJ Archives</h4>
