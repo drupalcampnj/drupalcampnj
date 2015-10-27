8f757000a5e86815af05ad993ffe79da6828f42f
Levi Sigworth <levis@alumni.cmu.edu>
2015-10-27 19:54:51 -0400

    Replace homepage banner image. Add icon font. Cleanup footer presentation.

--- docroot/sites/default/modules/custom/njblocks/templates/footer_contact_info.tpl.php
+++ docroot/sites/default/modules/custom/njblocks/templates/footer_contact_info.tpl.php
@@ -2,12 +2,12 @@
 
 <p>Get in touch with us if you are interested in sponsoring, speaking, or volunteering.</p>
 
-<ul>
+<ul class="contact">
 	<li><a href="tel:609-831-3265"><i title="icon-phone" class="icon fontello icon-phone" aria-hidden="true"></i><br>609-831-DCNJ(3265)</a></li>
 	<li><a href="mailto:team@drupalcampnj.org"><i title="icon-mail" class="icon fontello icon-mail" aria-hidden="true"></i><br>team@drupalcampnj.org</a></li>
 </ul>
 
-<ul>
+<ul class="community-links">
 	<li><a href="http://www.meetup.com/Central-NJ-Drupal-Group/"><i title="icon-comment" class="icon fontello icon-comment" aria-hidden="true"></i></a></li>
 	<li><a href="http://www.twitter.com/drupalcampnj/"><i title="icon-twitter" class="icon fontello icon-twitter" aria-hidden="true"></i></a></li>
 	<li><a href="https://groups.drupal.org/new-jersey"><i title="icon-drupal" class="icon fontello icon-drupal" aria-hidden="true"></i></a></li>
@@ -15,7 +15,7 @@
 
 <h4>DrupalCamp NJ Archives</h4>
 
-<ul>
+<ul class="archives">
 	<li><a href="http://drupalcampnj2015.drupalcamp.org">2015</a></li>
 	<li><a href="http://drupalcampnj2014.drupalcamp.org">2014</a></li>
 	<li><a href="http://drupalcampnj2013.drupalcamp.org">2013</a></li>
