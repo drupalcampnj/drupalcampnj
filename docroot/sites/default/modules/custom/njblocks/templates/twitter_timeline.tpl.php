d27beb85b4ef1536c0dd13b27a9e1b21e9bb5963
Sean T. Walsh <sean@crowdcg.com>
2015-10-29 18:56:17 -0400

    add twitter_timeline block

---
+++ docroot/sites/default/modules/custom/njblocks/templates/twitter_timeline.tpl.php
@@ -0,0 +1,4 @@
+<?php ?>
+
+<a class="twitter-timeline" href="https://twitter.com/drupalcampnj" data-widget-id="532370573221822464">Tweets by @drupalcampnj</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
