98c0dd1193b324eab1e41fd954fbda5eb404d98a
Levi Sigworth <levis@alumni.cmu.edu>
2015-11-01 19:43:27 -0500

    Add button classes.

--- docroot/sites/default/modules/custom/njblocks/templates/camp_highlights.tpl.php
+++ docroot/sites/default/modules/custom/njblocks/templates/camp_highlights.tpl.php
@@ -4,5 +4,5 @@
   <li class="camp">Camp: Saturday, January 30</li>
   <li class="mentoring">Mentoring &amp; Collaboration: Sunday, January 31</li>
 </ul>
-<a href="http://bit.ly/dcnj2016" class="register">Register</a> 
-<a href="/about" class="learn-more">Find Out More</a>
+<a href="http://bit.ly/dcnj2016" class="btn-action">Register</a> 
+<a href="/about" class="btn-info">Find Out More</a>
