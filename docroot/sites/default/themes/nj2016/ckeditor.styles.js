ab13ddc7e1f87cc7b51bf4afb074074015a70721
Sean T. Walsh <sean@crowdcg.com>
2015-11-02 22:45:57 -0500

    add image float styles to ckeditor

--- docroot/sites/default/themes/nj2016/ckeditor.styles.js
+++ docroot/sites/default/themes/nj2016/ckeditor.styles.js
@@ -24,27 +24,23 @@ if(typeof(CKEDITOR) !== 'undefined') {
 
             /* Object Styles */
 
-            //{
-            //    name : 'Image on Left',
-            //    element : 'img',
-            //    attributes :
-            //    {
-            //        'style' : 'padding: 5px; margin-right: 5px',
-            //        'border' : '2',
-            //        'align' : 'left'
-            //    }
-            //},
-            //
-            //{
-            //    name : 'Image on Right',
-            //    element : 'img',
-            //    attributes :
-            //    {
-            //        'style' : 'padding: 5px; margin-left: 5px',
-            //        'border' : '2',
-            //        'align' : 'right'
-            //    }
-            //}
+            {
+                name : 'Image on Left',
+                element : 'img',
+                attributes :
+                {
+                    'class' : 'image-left'
+                }
+            },
+
+            {
+                name : 'Image on Right',
+                element : 'img',
+                attributes :
+                {
+                    'class' : 'image-right'
+                }
+            },
             {
                 name : 'Red Button',
                 element : 'a',
