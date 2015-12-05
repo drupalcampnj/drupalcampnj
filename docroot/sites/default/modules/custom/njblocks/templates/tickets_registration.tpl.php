0f3f79b18d40c2c10662c0c544b99d7689b14b65
Levi Sigworth <levis@alumni.cmu.edu>
2015-12-05 14:33:06 -0500

    Make links for tickets to camp and training more clear and distinct.

--- docroot/sites/default/modules/custom/njblocks/templates/tickets_registration.tpl.php
+++ docroot/sites/default/modules/custom/njblocks/templates/tickets_registration.tpl.php
@@ -1,5 +1,5 @@
 <?php ?>
 
-<p class="description">(Includes breakfast, lunch, afternoon break, swag, and evening celebration.)</p>
+<p class="description">Tickets start at $30. (Includes breakfast, lunch, afternoon break, swag, and evening celebration.)</p>
 
 <a class="registration-link" href="http://bit.ly/dcnj2016">Register Now</a>
---
+++ docroot/sites/default/modules/custom/njblocks/templates/training_tickets.tpl.php
@@ -0,0 +1,5 @@
+<?php ?>
+
+<p class="description">A great value and low cost at $80.</p>
+
+<a class="registration-link" href="/program/trainings">Learn More</a>
