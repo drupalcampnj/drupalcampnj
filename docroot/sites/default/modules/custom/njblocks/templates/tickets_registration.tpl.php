5734d5e66ba562c7638935a69b901d0bf459cd2b
Levi Sigworth <levis@alumni.cmu.edu>
2016-01-05 10:09:06 -0500

    Update link for registration.

--- docroot/sites/default/modules/custom/njblocks/templates/tickets_registration.tpl.php
+++ docroot/sites/default/modules/custom/njblocks/templates/tickets_registration.tpl.php
@@ -2,4 +2,4 @@
 
 <p class="description">Tickets start at $30. (Includes breakfast, lunch, afternoon break, swag, and evening celebration.)</p>
 
-<a class="registration-link" href="http://bit.ly/dcnj2016">Register Now</a>
+<a class="registration-link" href="https://www.eventbrite.com/tickets-external?eid=18590166689">Register Now</a>
