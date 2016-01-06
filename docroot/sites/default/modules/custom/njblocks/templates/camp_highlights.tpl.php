fe34184a2ef1ce88c31c21b0c63fc4934ec46fa4
Levi Sigworth <levis@alumni.cmu.edu>
2016-01-05 21:50:08 -0500

    Update link destinations for registration.

--- docroot/sites/default/modules/custom/njblocks/templates/camp_highlights.tpl.php
+++ docroot/sites/default/modules/custom/njblocks/templates/camp_highlights.tpl.php
@@ -5,6 +5,6 @@
   <li class="mentoring">Mentoring &amp; Collaboration: Sunday, January 31</li>
 </ul>
 <div class="actions">
-  <a href="https://www.eventbrite.com/tickets-external?eid=18590166689" class="btn-action">Register</a>
+  <a href="/saturday-camp-tickets" class="btn-action">Register</a>
   <a href="/about" class="btn-info">Find Out More</a>
 </div>
