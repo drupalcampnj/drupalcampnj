2d7aab1816ff61bd7a74c558be613cf2d3492bc7
Levi Sigworth <levis@alumni.cmu.edu>
2016-01-08 17:56:11 -0500

    Show primary speaker on Session detail page.

--- docroot/sites/default/themes/nj2016/template.php
+++ docroot/sites/default/themes/nj2016/template.php
@@ -1,13 +1,8 @@
 <?php
 
 /**
- * Implements template_preprocess_page().
+ * Implements template_preprocess_node().
  */
-function cbii_preprocess_page(&$variables) {
-  
-// Add JS and CSS for meanmenu
-  if ($path = libraries_get_path('meanmenu')) {
-    drupal_add_js($path . '/jquery.meanmenu.js');
-    drupal_add_js(drupal_get_path('theme', 'cbii') . '/js/meanmenu.js');
-  }
+function nj2016_preprocess_node(&$vars) {
+  $vars['submitted'] = t('By !username', array('!username' => $vars['name']));
 }
