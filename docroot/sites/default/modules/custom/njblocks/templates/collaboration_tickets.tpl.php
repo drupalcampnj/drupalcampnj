a4f03c0d4bd3e899e84d32e43cff7ae7ab1ab78f
Levi Sigworth <levis@alumni.cmu.edu>
2016-01-28 16:02:37 -0500

    Swap Friday training tickets block for Sunday Mentoring and Collaboration block.

---
+++ docroot/sites/default/modules/custom/njblocks/templates/collaboration_tickets.tpl.php
@@ -0,0 +1,5 @@
+<?php ?>
+
+<p class="description">Contribute to Drupal, Learn about Drupal 8, and work on what you love</p>
+
+<a class="btn-reverse" href="/program/mentoring-collaboration">Learn More</a>
