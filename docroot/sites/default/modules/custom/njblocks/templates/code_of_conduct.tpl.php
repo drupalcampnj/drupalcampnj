af383202ed3510f68c9bacec505b66928bf1d7a0
Levi Sigworth <levis@alumni.cmu.edu>
2016-01-28 15:17:06 -0500

    Add Code of Conduct block.

---
+++ docroot/sites/default/modules/custom/njblocks/templates/code_of_conduct.tpl.php
@@ -0,0 +1,4 @@
+<?php ?>
+<p class="description">In order to ensure a positive and welcoming evironment for all, attendees should review and respect the code of conduct.</p>
+
+<a href="/code-conduct" class="btn-reverse">Find Out More</a>
