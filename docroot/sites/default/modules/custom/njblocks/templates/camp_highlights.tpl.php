8ed6877717b0e9ae0b8fd27e7ecfe80119e5d880
Sean T. Walsh <sean@crowdcg.com>
2016-09-06 09:53:45 -0400

    fix error on homepage

--- docroot/sites/default/modules/custom/njblocks/templates/camp_highlights.tpl.php
+++ docroot/sites/default/modules/custom/njblocks/templates/camp_highlights.tpl.php
@@ -2,7 +2,7 @@
 <ul>
   <li class="training">Training: Friday, February 3</li>
   <li class="camp">Camp: Saturday, February 4</li>
-  <li class="mentoring">Mentoring &amp;amp; Collaboration: Sunday, February 5</li>
+  <li class="mentoring">Mentoring &amp; Collaboration: Sunday, February 5</li>
 </ul>
 <div class="actions">
   <a href="/saturday-camp-tickets" class="btn-action">Register</a>
